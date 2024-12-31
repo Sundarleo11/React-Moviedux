2dbc831e72090aeaa4999fb54236890997d1f49c
Sundarleo11 <sundarrock1999@gamil.com>
2024-12-31 18:29:36 +0530

    rating interpolation

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,13 +8,20 @@ const handlerError=(e)=>{
 
     e.target.src="images/default.jpg";
 };
+
+const getRatingClass=(rating)=>{
+    if(rating>=8) return "rating-good";
+    if(rating>5 && rating<8) return "rating-ok";
+    return "rating-bad";
+
+}
     return(
         <div key={movie.id} className="movie-card" onError={handlerError}>
         <img src={`images/${movie.image}`} alt={movie.title}></img>
         <div className="movie-card-info">
         <p className="movie-card-title">{movie.title}</p>
         <p className="movie-card-genre">{movie.genre}</p>
-        <p className="movie-card-rating">{movie.rating}</p>
+        <p className={`movie-card-rating, ${getRatingClass(movie.rating)}`}>{movie.rating}</p>
        
       </div>
     </div>

eb02962407bcc47d749a4a4949f4817a347e2558
Sundarleo11 <sundarrock1999@gamil.com>
2024-12-31 19:01:17 +0530

    search bar

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,27 +1,40 @@
-import React,{useState,useEffect} from "react";
-import '../styles.css';
+import React, { useState, useEffect } from "react";
+import "../styles.css";
 import MovieCard from "./MovieCard";
 
-export default function MoviesGrid(){
+export default function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
 
-    const [movies,setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(()=>{
-       
-        fetch("movies.json")
-        .then(res=>res.json())
-        .then(data=>setMovies(data))
-       
-    },[]);
+  const handlerSeachTerm = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
+  const fliterMovie = movies.filter((e) =>
+    e.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-    return(<div className="movies-grid">
-        {
-           movies.map(movie=>(
-           <MovieCard movie={movie} key={movie.id}></MovieCard>
-           ))
-        }
+  useEffect(() => {
+    fetch("movies.json")
+      .then((res) => res.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="search word..."
+        value={searchTerm}
+        onChange={handlerSeachTerm}
+      />
+      <div className="movies-grid">
+        {fliterMovie.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
     </div>
-    
-)
+  );
 }
